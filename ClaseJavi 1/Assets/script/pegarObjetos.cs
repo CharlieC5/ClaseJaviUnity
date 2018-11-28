90c6300678faeec035c6ac905a07581f019f6f77
CarlosCamacho <carcasi_5@hotmail.com>
2018-11-28 18:39:06 +0100

    subidaProyecto

---
+++ ClaseJavi 1/Assets/script/pegarObjetos.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pegarObjetos : MonoBehaviour {
+
+    Rigidbody2D Objeto;
+
+	// Use this for initialization
+	void Start () {
+
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKeyDown(KeyCode.Space)){
+
+            Objeto.simulated = true;
+            Objeto.AddForce(new Vector2(150f, 150f));
+            transform.DetachChildren();
+
+        }
+        
+		
+	}
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+        if(collision.gameObject.CompareTag("Objeto")){
+
+            Objeto = collision.gameObject.GetComponent<Rigidbody2D>();
+            Objeto.simulated = false;
+            collision.transform.parent = transform;
+            
+        }
+	}
+}
